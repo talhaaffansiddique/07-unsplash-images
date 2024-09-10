10df59208db3f45d6c31ddd3292cf97a39dfd61d
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-09-10 17:34:47 +0500

    327 - dark theme CSS

---
+++ src/context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", newDarkTheme);
+    console.log(body);
+  };
+
+  return (
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
