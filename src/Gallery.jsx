3f8719a7dc401135396a210bace5b88ae1e8b30a
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-09-11 17:51:24 +0500

    334. Setup Query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=7Q4EfAl_GiXIFPtDzRyW1_v_QuNpjWiPGPHi9Qa3DKY&query=office";
 
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
+
   return <div>Gallery</div>;
 };
 
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit"></button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
